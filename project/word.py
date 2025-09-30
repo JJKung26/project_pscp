a351a70954864e87195d642f26958bed3c999a1b
Jetsada <jetsada.jet1994@gmail.com>
2025-10-01 00:09:09 +0700

    เกือบfinal

--- project/word.py
+++ project/word.py
@@ -1,16 +1,16 @@
 WORDS_BY_LEVEL = {
-        "animals": ["PANDA", "SHEEP", "HORSE", "GOOSE", "EAGLE", "ZEBRA", "WHALE","HIPPO", "RHINO", "SNAIL", "HYENA", "SHARK", "SLOTH"],
-        "fruits":  ["APPLE", "MANGO", "GRAPE", "LEMON", "PEACH", "MELON", "OLIVE", "BERRY", "GUAVA", "PLUMS", "COCOA"],
-        "objects":   ["TABLE", "CHAIR", "PHONE", "CLOCK", "KNIFE", "PENAL", "GLASS", "PLATE", "RADIO", "SPOON", "BRUSH", "PAPER", "SOFAS"],
-        "instruments": ["AUDIO", "PIANO", "MUSIC", "SOUND", "DRUMS", "STAGE", "DANCE", "ALBUM", "RHYME", "REMIX", "MIXER", "OPERA", "BANDS"],
-        "colors":  ["GREEN", "BLACK", "BROWN", "WHITE", "CREAM", "LEMON", "PEACH", "BEIGE", "AZURE", "AMBER"],
-        "body_parts": ["HEART", "BRAIN", "TOOTH", "KNEES", "ELBOW", "THUMB", "WRIST", "ANKLE", "LIVER", "SPINE"],
-        "food": ["PIZZA", "CANDY", "SALAD", "SUUSHI", "HONEY", "STEAK", "CREAM", "JUICE", "SAUCE", "PASTA", "TOAST", "SUGAR", "WATER", "BREAD"],
-        "countrys": ["JAPAN", "CHINA", "INDAI", "EGYPT", "PARIS", "SPAIN", "KOREA", "NEPAL", "DUBAI", "QATRA", "CHILE", "MILAN"],
-        "jobs": ["NURSE", "PILOT", "GUARD", "MINER", "ACTOR", "AGENT", "COACH"],
-        "sports": ["CHESS", "RUGBY", "SKATE", "SWIMS", "GOLFS", "SAILS"],
-        "nature": ["RIVER", "OCEAN", "BEACH", "STORM", "CLOUD", "STONE", "RAINY", "WINDS", "FLAWE", "ROCKS", "MOUNT", "FLORA"],
-        "time": ["MONTH", "HOURS", "EARLY", "NIGHT", "DATES", "EVENT", "LATER"],
-        "emotions": ["HAPPY", "ANGRY", "PROUD", "SCARE", "SORRY", "WORRY", "RELAX", "SHOCK", "GLORY"],
-        "music": ["PIANO", "GUITAR", "SAXON", "TRUMP", "BASSO", "CELLO", "FLUTE", "DRUMS", "VOICE", "CHORD", "NOTES", "LYRIC", "SCALE", "OPERA"]
-    }
+    "animals": ["PANDA", "SHEEP", "HORSE", "GOOSE", "EAGLE", "ZEBRA", "WHALE", "HIPPO", "RHINO", "SNAIL", "HYENA", "SHARK", "SLOTH"],
+    "fruits": ["APPLE", "MANGO", "GRAPE", "LEMON", "PEACH", "MELON", "OLIVE", "BERRY", "GUAVA", "PLUMS"],
+    "objects": ["TABLE", "CHAIR", "PHONE", "CLOCK", "KNIFE", "GLASS", "PLATE", "RADIO", "SPOON", "BRUSH", "PAPER", "SOFAS"],
+    "instruments": ["AUDIO", "PIANO", "MUSIC", "SOUND", "DRUMS", "STAGE", "DANCE", "ALBUM", "RHYME", "REMIX", "MIXER", "OPERA", "BANDS"],
+    "colors": ["GREEN", "BLACK", "BROWN", "WHITE", "CREAM", "LEMON", "PEACH", "BEIGE", "AZURE", "AMBER"],
+    "body_parts": ["HEART", "BRAIN", "TOOTH", "KNEES", "ELBOW", "THUMB", "WRIST", "ANKLE", "LIVER", "SPINE"],
+    "food": ["PIZZA", "CANDY", "SALAD", "HONEY", "STEAK", "CREAM", "JUICE", "SAUCE", "PASTA", "TOAST", "SUGAR", "WATER", "BREAD"],
+    "countries": ["JAPAN", "CHINA", "EGYPT", "SPAIN", "KOREA", "NEPAL", "CHILE", "ITALY", "WALES", "QATAR"],
+    "jobs": ["NURSE", "PILOT", "GUARD", "MINER", "ACTOR", "AGENT", "COACH"],
+    "sports": ["CHESS", "RUGBY", "SKATE", "DARTS", "RODEO", "BOXING"],
+    "nature": ["RIVER", "OCEAN", "BEACH", "STORM", "CLOUD", "STONE", "RAINY", "WINDS", "ROCKS", "MOUNT", "FLORA"],
+    "time": ["MONTH", "HOURS", "EARLY", "NIGHT", "DATES", "EVENT", "LATER"],
+    "emotions": ["HAPPY", "ANGRY", "PROUD", "WORRY", "SORRY", "SHOCK", "GLORY"],
+    "music": ["PIANO", "CELLO", "FLUTE", "DRUMS", "VOICE", "CHORD", "NOTES", "LYRIC", "SCALE", "OPERA"]
+}
